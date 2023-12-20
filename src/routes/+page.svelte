0184944fbdb14b42cdc9b6325164883c4ac2cd1c
KittenzExe <67358250+KittenzExe@users.noreply.github.com>
2023-12-20 08:39:30 +0800

    fix?

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,7 +20,7 @@
 </style>
 
 <body class="w-screen h-screen overflow-hidden relative">
-  <img id="backgroundImage" class="absolute w-screen h-screen" alt="arona blue archive" src="./__arona_blue_archive_drawn_by_pla0658__0e36463634a509021cc8f707536b30c6.webp" style="object-fit: cover; transform-origin: 50% 50%;">
+  <img id="backgroundImage" class="absolute w-screen h-screen" alt="arona blue archive" src="/__arona_blue_archive_drawn_by_pla0658__0e36463634a509021cc8f707536b30c6.webp" style="object-fit: cover; transform-origin: 50% 50%;">
   <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
     <p class="text-3xl font-semibold text-sky-900">Redirecting...</p>
     <p class="font-semibold text-sky-900">(If you are not redirected, <span><a href="https://bsky.app/profile/kittenzexe.bsky.social">click here</a></span>)</p>
