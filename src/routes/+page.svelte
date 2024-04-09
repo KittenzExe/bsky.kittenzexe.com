b6231799f1b36b43c693ea9885f155b0032536de
KittenzExe <67358250+KittenzExe@users.noreply.github.com>
2024-04-09 08:19:51 +0800

    feat: MANUKA!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,16 +13,16 @@
 <style>
   @media (orientation: landscape) {
     #backgroundImage {
-      transform: translateY(-40%);
+      transform: translateY(-20%);
       height: 175%;
     }
   }
 </style>
 
 <body class="w-screen h-screen overflow-hidden relative">
-  <img id="backgroundImage" class="absolute w-screen h-screen" alt="arona blue archive" src="/__arona_blue_archive_drawn_by_pla0658__0e36463634a509021cc8f707536b30c6.webp" style="object-fit: cover; transform-origin: 50% 50%;">
-  <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
-    <p class="text-3xl font-semibold text-sky-900">Redirecting...</p>
-    <p class="font-semibold text-sky-900">(If you are not redirected, <span><a href="https://bsky.app/profile/kittenzexe.bsky.social">click here</a></span>)</p>
-  </div>
+    <img id="backgroundImage" class="absolute w-full h-full object-cover object-left" alt="manuka!" src="/manuka.webp">
+    <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
+      <p class="text-3xl font-semibold text-gray-500">Redirecting...</p>
+      <p class="font-semibold text-gray-500">(If you are not redirected, <span><a href="https://bsky.app/profile/kittenzexe.bsky.social">click here</a></span>)</p>
+    </div>
 </body>
